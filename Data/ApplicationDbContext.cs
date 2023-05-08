afad273a50d5cace5690b0298a3087bc7e868dc7
Kamil <k>
2023-05-08 13:52:03 +0200

    Instal application for Sitecore Exercise
    - first commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SiteCoreProjectsExercise.Models;
+
+namespace SiteCoreProjectsExercise.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option) : base(option)
+        {
+        }
+        
+
+    }
+}
